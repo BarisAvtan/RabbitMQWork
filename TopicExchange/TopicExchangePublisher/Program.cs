6ec0538f0761156bc0fbba61d2ff58e2e765e444
Barış Avtan <baris.avtan@arvento.com>
2023-04-01 01:19:23 +0300

    add topic excahange project

---
+++ TopicExchange/TopicExchangePublisher/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+
+        var factory = new ConnectionFactory();
+
+        factory.Uri = new Uri("amqps://toggqveh:poj3d4VTUAOnf_T8YLTWdApegWH5OOTV@shark.rmq.cloudamqp.com/toggqveh");
+        
+        using var connection = factory.CreateConnection();
+
+        var channel = connection.CreateModel();
+
+        channel.ExchangeDeclare("logs-topic", durable: true, type: ExchangeType.Topic);
+
+        Random rnd = new Random();
+
+        Enumerable.Range(1, 50).ToList().ForEach(x =>
+        {
+
+            LogNames log1 = (LogNames)rnd.Next(1, 5);
+            LogNames log2 = (LogNames)rnd.Next(1, 5);
+            LogNames log3 = (LogNames)rnd.Next(1, 5);
+
+            var routeKey = $"{log1}.{log2}.{log3}";
+            string message = $"log-type: {log1}-{log2}-{log3}";
+            var messageBody = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish("logs-topic", routeKey, null, messageBody);
+
+            Console.WriteLine($"Log gönderilmiştir : {message}");
+
+        });
+
+        Console.ReadLine();
+
+public enum LogNames
+{
+    Critical = 1,
+    Error = 2,
+    Warning = 3,
+    Info = 4
+}
+
+
---
+++ TopicExchange/TopicExchangeSubsriber/Program.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+
+factory.Uri = new Uri("amqps://toggqveh:poj3d4VTUAOnf_T8YLTWdApegWH5OOTV@shark.rmq.cloudamqp.com/toggqveh");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.BasicQos(0, 1, false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+var queueName = channel.QueueDeclare().QueueName;
+
+//var routekey = "*.Error.*";//iiçinde eror geçenler
+
+//var routekey = "*.*.Warning";//sonu sadece warning olanlar
+
+var routekey = "Info.#";//başı info olsun sonu önemli değil
+
+channel.QueueBind(queueName, "logs-topic", routekey);
+
+channel.BasicConsume(queueName, false, consumer);
+
+Console.WriteLine("Loglar dinleniyor...");
+
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+    Thread.Sleep(1500);
+
+    Console.WriteLine("Gelen Mesaj:" + message);
+
+    File.AppendAllText("log-critical.txt", message+ "\n");
+
+    channel.BasicAck(e.DeliveryTag, false);
+};
+
+
+
+
+
+Console.ReadLine();
